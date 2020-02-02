f7cd17e9942db2e0d47be4d51a04494014360b0d
tnichell <56081527+tnichell@users.noreply.github.com>
2020-02-02 15:31:55 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/fdf.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fdf.dir/fdf.c.o"
+  "CMakeFiles/fdf.dir/main.c.o"
+  "CMakeFiles/fdf.dir/read_file.c.o"
+  "fdf"
+  "fdf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/fdf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
